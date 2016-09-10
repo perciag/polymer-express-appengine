0d771eae293b351262992dd312ff6649d5b42fb9
Percival Adriano <percival@crius.com.br>
2016-09-10 14:25:59 -0400

    update project routes

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -21,9 +20,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +56,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 module.exports = app;
